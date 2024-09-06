ef0af712e764d2191b4abe145d35414388bb9040
DrCosmo98 <samuel.w.r.hayes98@gmail.com>
2024-09-06 21:01:33 +0100

    classes are now engaged

---
+++ account_handler.py
@@ -0,0 +1,71 @@
+class account_handler:
+    username = None
+    password = None
+
+    def __init__(self):
+        print("username")
+    
+    def password(self):
+        print("password")
+    
+    #method that asigns new username    
+    def enter_username(self):
+        self.username = input("enter new username: \n" )
+        print(self.username + " is now your username, welcome \n")
+        
+    #method that asigns new password and verfiy
+    def enter_password(self):
+        self.password = input("enter new password. Must contain, lower case, upper case, special character, number: \n")
+        
+        lower_case = False
+        upper_case = False
+        num = False
+        special = False
+
+        for char in self.password:
+            
+            if char.islower():
+                num = True
+            if char.isupper():
+                lower_case = True
+            if char.isdigit():
+                upper_case = True
+            if not char.isalnum():
+                special = True
+        if lower_case and upper_case and num and special:
+            print("vaild password ")
+            return True
+                
+        else:
+            print("bad ")
+            return False
+    
+    #check login
+    def check_username(self):
+        username = input("Enter username: \n")
+        if username != self.username:
+            print("User name " + username + " does not match records")
+            return False
+        else:
+            return True
+        
+    #check passward login
+    def check_password(self):
+        password_attempts = 3
+        password = None
+        while password != self.password:
+            password = input(str(password_attempts) + " attemts at password \nEnter password: \n")
+            password_attempts -= 1
+            if password_attempts == 0:
+                print("get out you dam hacker")
+                exit()
+        return True
+          
+        
+
+        
+            
+        
+        
+        
+    
--- main.py
+++ main.py
@@ -1,125 +1,25 @@
-#define my global username and password variables
-username = None
-password = None
+from account_handler import account_handler
 
-#method that asigns new username
-def enter_username(new_username):
-    global username # modifying the global variable
-    username = new_username
-    print(username + " is now your user name welcome \n")
-    
-    #doesnt need a return as there is no value to return as it is just setting the username
-    
-#method that asigns new password and verfiy 
-def enter_password(new_password):
-    global password # modifying the global variable
-
-    #validtation of password
-    while True:
-        if len(new_password) > 8 and len(new_password) < 15:
-            
-            #set all checks to flase to start with
-            lower_case = False
-            upper_case = False
-            num = False
-            special = False
-            
-            # checking if it contains the following has at lease 1 number, upper, lower and special char            
-            for char in new_password:
-                if(char.isdigit()):
-                    num = True
-                if(char.islower()):
-                    lower_case = True
-                if(char.isupper()):
-                    upper_case = True
-                    
-                # check if it has at least one char that is not a letter or num
-                if(not char.isalnum()): 
-                    special = True
-            if lower_case and upper_case and num and special:#replaced the return statement to if statement as the return was ending the loop
-                #assign the new_password as the global password
-                password = new_password
-                print(" your password is now set \n")
-                
-                #print for testing purposes
-                print(password)
-                return
-            
-            #this reloops to ask for a new input from the user
-            
-            else: 
-                continue
-                #do i really need this?
-        else:
-                new_password = input("password must contain, lower case, upper case, special character, number. must be between 8 - 15 characters. try again \n")
-    
- #checks if user name is correct
-def check_login(checked_login):
-    
-    #sets a loop to check the user name matches
-    while checked_login != username: 
-        failed_input = input("type 'try again' or type 'exit' to quit: \n")
-        
-        if failed_input.lower() == "try again":
-            checked_login = input("Enter username: \n")
-        
-        elif failed_input.lower() == "exit":
-            print("Exiting login process.")
-            return False  # Return False to indicate the login failed
-        
-        else:
-            print("Invalid option.")
-    
-    checked_password = input("Enter password: \nyou have 3 attempts \n ")
-    password_attempts = 3
-
-    while checked_password != password and password_attempts > 0: #sets a loop to check the user name matches
-        password_attempts -= 1
-        print("you have " + str(password_attempts) + " left ")
-        checked_password = input("Enter password: \n")
-
-
-        if password_attempts == 1:
-            print("get out you dam hacker")
-            exit()     
-                            
-        #failed_input = input("wrong password type 'try again' or type 'exit' to quit: ")
-        
-        #if failed_input.lower() == "try again":
-            #checked_password = input("Enter password: ")
-            #continue
-       
-        #elif failed_input.lower() == "exit":
-            #print("Exiting login process.")
-            #return False  # Return False to indicate the login failed
-        
-        #else:
-            #print("Invalid option.")
-
-    print(checked_login + " logged in")
-    print(type(checked_login))
-    return True # Return True to indicate a successful login
+ah = account_handler()
+ah.password()
 
 while True:
-    reg_status = input('enter the following "login" or "sign up" or "exit" \n') 
+    reg_status = input('enter the following "l" or "s" or "exit" \n') 
     
-    if reg_status == "sign up":
-        new_username = input("enter new username: \n" ) 
-        enter_username(new_username)
-        new_password = input("enter new password. Must contain, lower case, upper case, special character, number. must be between 8 - 15 characters: \n")
-        enter_password(new_password)
-        continue
-    
-    #this it the login step
-    elif reg_status == "login":
-        checked_login = input("enter username: ")
+    if reg_status == "s":
         
-        if check_login(checked_login): #same as if check_username(checked_username) == True:
-           break
-
-    #added exit
+        ah.enter_username()
+        while True:
+           if ah.enter_password() == True:
+               break
+    elif reg_status == "l":
+        while True:
+            if ah.check_username() == True:
+                break
+        ah.check_password()
+        print(ah.username + " logged in")
     elif reg_status == "exit":
-        break
+        exit()
     
     else:
         print("Invalid option. Try again. \n")
